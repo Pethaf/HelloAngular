964e892bf336d1c2316c151b7f3de35d496eb3d9
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-28 17:42:51 +0200

    Add home component and about component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent} from './components/navbar/navbar.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { AboutComponent } from './components/home/about.component';
+import { HomeComponent } from './components/home/home.component';
+
 @NgModule({
     imports: [ BrowserModule],
-    declarations: [AppComponent, NavbarComponent, JumbotronComponent],
+    declarations: [AppComponent, NavbarComponent, JumbotronComponent, HomeComponent],
     bootstrap: [AppComponent]
 })
 
