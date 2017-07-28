c25c84ebd6616e0f5d136bda9ced0f1e613db51f
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-28 16:17:52 +0200

    Add selector for navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>My First Angular App</h1><navbar></navbar>'
 })
 
 export class AppComponent 
